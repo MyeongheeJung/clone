69b76c98f6bceca0f9876b3394bb54455554c84d
MyeongheeJung <dobby.jmh@gmail.com>
2022-10-21 18:48:08 +0900

    cyworld

---
+++ cyworld/game.js
@@ -0,0 +1,18 @@
+const startWard = () => {
+  let myword = document.getElementById('myword').value
+  let word = document.getElementById('word').innerText
+
+  let lastword = word[word.length - 1]
+  let firstword = myworld[0]
+
+  if (firstword === lastword) {
+    // 정답일 때
+    document.getElementById('result').innerText = '정답입니다!'
+    document.getElementById('word').innerText = myword
+    document.getElementById('myworld').value = ''
+  } else {
+    // 오답일 때
+    document.getElementById('result').innerText = "땡!"
+    document.getElementById('myworld').value = ''
+  }
+}
